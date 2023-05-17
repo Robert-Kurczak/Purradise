c8d1a1fb9b63dd24f59ebbe0b3e5e2992fe9ad6e
Robert Kurczak <robertkurczak.studia@gmail.com>
2023-05-18 01:33:36 +0200

    Placing structure UI

--- Assets/Scripts/Structures/StructurePlacer.cs
+++ Assets/Scripts/Structures/StructurePlacer.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class StructurePlacer : MonoBehaviour{
 	public GameObject structuresParent;
 	public GameObject structure;
-	// public Material placingMaterial;
-	// public Material normalMaterial;
+	public GameObject optionsCanvas;
 
 	public Color32 allowColor;
 	public Color32 disallowColor;
 
 	private bool placingMode = false;
 	private CollidersCounter collidersCounter;
+	private GameObject instantiatedCanvas;
 	private GameObject instantiatedStructure;
 
 	private void changeColor(Color32 color){
@@ -33,6 +34,22 @@ public class StructurePlacer : MonoBehaviour{
 		instantiatedStructure = Instantiate(structure, transform.position, transform.rotation, structuresParent.transform);
 		collidersCounter = instantiatedStructure.GetComponent<CollidersCounter>();
 
+		//---Setting up options canvas---
+		Vector3 optionsPosition = new Vector3(
+			instantiatedStructure.transform.position.x,
+			instantiatedStructure.transform.position.y + 3.62f,
+			instantiatedStructure.transform.position.z
+		);
+
+		instantiatedCanvas = Instantiate(optionsCanvas, optionsPosition, transform.rotation, instantiatedStructure.transform);
+
+		GameObject abortButton = instantiatedCanvas.transform.GetChild(0).gameObject;
+		GameObject confirmButton = instantiatedCanvas.transform.GetChild(1).gameObject;
+
+		abortButton.GetComponent<Button>().onClick.AddListener(abortPlacing);
+		confirmButton.GetComponent<Button>().onClick.AddListener(placeStructure);
+		//------
+
 		foreach(Transform child in instantiatedStructure.transform){
 			if(child.TryGetComponent<BoxCollider2D>(out BoxCollider2D collider))
 				collider.isTrigger = true;
@@ -40,13 +57,23 @@ public class StructurePlacer : MonoBehaviour{
 		placingMode = true;
 	}
 
+	private void abortPlacing(){
+		Debug.Log("aborting placing");
+	}
+
+	private void placeStructure(){
+		Debug.Log("placed");
+	}
+
 	void Update(){
 		if(placingMode){
-			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			instantiatedStructure.transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
+			if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+				Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+				instantiatedStructure.transform.position = new Vector3(newPosition.x, newPosition.y, 0);
 
-			if(collidersCounter.isColliding()) changeColor(disallowColor);
-			else changeColor(allowColor);
+				if(collidersCounter.isColliding()) changeColor(disallowColor);
+				else changeColor(allowColor);
+			}
 		}
 	}
 }
